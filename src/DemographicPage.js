506e3361125474957953ec92f0b847d27951e318
saadshakeel1357 <saadshakeel1357@gmail.com>
2024-05-07 17:14:10 +0200

    added designs to DemographicPage

--- src/DemographicPage.js
+++ src/DemographicPage.js
@@ -1,119 +1,155 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Import Link from React Router
+import { useLocation } from 'react-router-dom'; // Import useLocation hook from React Router
+import videoBg from "./components/bgvideo.mp4";
+import './App.css';
+import './styling/DemographicPage.css'; // Import CSS file for styling
+
 
 // DemographicPage component
 const DemographicPage = () => {
-  // State to manage form data and validity
-  const [formData, setFormData] = useState({
-    age: '',
-    gender: '',
-    education: '',
-    field: ''
-  });
-  const [formValid, setFormValid] = useState(false); // Track form validity
-  const navigate = useNavigate();
+	// State to manage form data and validity
+	const [formData, setFormData] = useState({
+		age: '',
+		gender: '',
+		education: '',
+		field: ''
+	});
+	const [formValid, setFormValid] = useState(false); // Track form validity
+	const navigate = useNavigate();
 
-  // Effect to check form validity on form data change
-  useEffect(() => {
-    // Check if all form fields are filled
-    const isFormValid = Object.values(formData).every(value => value.trim() !== '');
-    setFormValid(isFormValid);
-  }, [formData]);
+	// Effect to check form validity on form data change
+	useEffect(() => {
+		// Check if all form fields are filled
+		const isFormValid = Object.values(formData).every(value => value.trim() !== '');
+		setFormValid(isFormValid);
+	}, [formData]);
 
-  // Handler for input change
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
+	// Handler for input change
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value
+		});
+	};
 
-  // Handler for form submission
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  	// Handler for form submission
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-    if (formValid) {
-      // Save form data to sessionStorage
-      sessionStorage.setItem('formData', JSON.stringify(formData));
-      
-      // // Create a Blob object containing the JSON data
-      // const blob = new Blob([JSON.stringify(formData)], { type: 'application/json' });
+		if (formValid) {
+			// Save form data to sessionStorage
+			sessionStorage.setItem('formData', JSON.stringify(formData));
+		
+			// // Create a Blob object containing the JSON data
+			// const blob = new Blob([JSON.stringify(formData)], { type: 'application/json' });
 
-      // // Create a temporary anchor element to download the JSON file
-      // const anchor = document.createElement('a');
-      // anchor.href = URL.createObjectURL(blob);
-      // anchor.download = 'formData.json';
+			// // Create a temporary anchor element to download the JSON file
+			// const anchor = document.createElement('a');
+			// anchor.href = URL.createObjectURL(blob);
+			// anchor.download = 'formData.json';
 
-      // // Trigger the download
-      // anchor.click();
+			// // Trigger the download
+			// anchor.click();
 
-      // // Clean up
-      // URL.revokeObjectURL(anchor.href);
+			// // Clean up
+			// URL.revokeObjectURL(anchor.href);
 
-      navigate('/instructions'); // Navigate to the next page
-    } else {
-      alert('Please fill in all required fields.');
-    }
-  };
+			navigate('/instructions'); // Navigate to the next page
+		} else {
+			alert('Please fill in all required fields.');
+		}
 
-  return (
-    <div>
-      {/* Demographics header */}
-      <p style={{ fontSize: '45px' }}>Demographics</p>
-      {/* Form */}
-      <form onSubmit={handleSubmit}>
-        <div style={{ height: '150px', overflowY: 'auto', border: '1px solid white', borderRadius: '5px', padding: '10px', width: '450px' }}>
-          {/* Individual form fields */}
-          <div>
-            <label htmlFor="age" style={{ fontSize: '18px' }}>Age:  </label>
-            <select id="age" name="age" value={formData.age} onChange={handleInputChange} style={{ fontSize: '10px' }}>
-              <option value="">Select</option>
-              <option value="underage">18 below</option>
-              <option value="young">18-25</option>
-              <option value="midage">26-40</option>
-              <option value="old">41 above</option>
-              {/* <option value="other">Other</option> */}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="gender" style={{ fontSize: '18px' }}>Gender:  </label>
-            <select id="gender" name="gender" value={formData.gender} onChange={handleInputChange} style={{ fontSize: '10px' }}>
-              <option value="">Select</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              {/* <option value="other">Other</option> */}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="education" style={{ fontSize: '18px' }}>Education Level:  </label>
-            <select id="education" name="education" value={formData.education} onChange={handleInputChange} style={{ fontSize: '10px' }}>
-              <option value="">Select</option>
-              <option value="Bachelor">Bachelor</option>
-              <option value="Master">Master</option>
-              <option value="PhD">PhD</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="field" style={{ fontSize: '18px' }}>Field of Study:  </label>
-            <select id="field" name="field" value={formData.field} onChange={handleInputChange} style={{ fontSize: '10px' }}>
-              <option value="">Select</option>
-              <option value="cs">Computer Science</option>
-              <option value="finance">Finance</option>
-              <option value="psychology">Psychology</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
+	};
+
+	const location = useLocation();
+	const searchParams = new URLSearchParams(location.search);
+	const randomId = searchParams.get('randomId');
+
+  	return (
+		<div>
+
+			<video autoPlay loop muted playsInline style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', opacity:'0.8' }} playbackRate={0.25} >
+			<source src={videoBg} type="video/mp4"></source>
+			</video>
+
+			{/* Participant ID */}
+			<button className="user-id">{randomId}</button>
+
+			{/* VibeVision text */}
+			<div className="vibe-vision">VibeVision</div>
+		
+			{/* Container for elements */}
+			<div className="container">
+				{/* Welcome text */}
+				<div className="demographics">Demographics</div>
+
+				<div className="form-box">
+
+					<div className='form'>
+					{/* Form */}
+					<form onSubmit={handleSubmit}>
+					{/* <div style={{ height: '150px', overflowY: 'auto', border: '1px solid white', borderRadius: '5px', padding: '10px', width: '450px' }}> */}
+						{/* Individual form fields */}
+						<div >
+							<label htmlFor="age" >Age:  </label>
+							<select id="age" name="age" value={formData.age} onChange={handleInputChange} className='dropdown'>
+								<option value="">Select</option>
+								<option value="underage">18 below</option>
+								<option value="young">18-25</option>
+								<option value="midage">26-40</option>
+								<option value="old">41 above</option>
+								{/* <option value="other">Other</option> */}
+							</select>
+						</div>
+
+						<div >
+							<label htmlFor="gender" >Gender:  </label>
+							<select id="gender" name="gender" value={formData.gender} onChange={handleInputChange} className='dropdown'>
+								<option value="">Select</option>
+								<option value="male">Male</option>
+								<option value="female">Female</option>
+								{/* <option value="other">Other</option> */}
+							</select>
+						</div>
+
+						<div >
+							<label htmlFor="education" >Education Level:  </label>
+							<select id="education" name="education" value={formData.education} onChange={handleInputChange} className='dropdown'>
+								<option value="">Select</option>
+								<option value="Bachelor">Bachelor</option>
+								<option value="Master">Master</option>
+								<option value="PhD">PhD</option>
+							</select>
+						</div>
+
+						<div>
+							<label htmlFor="field" >Field of Study:  </label>
+							<select id="field" name="field" value={formData.field} onChange={handleInputChange} className='dropdown'>
+								<option value="">Select</option>
+								<option value="cs">Computer Science</option>
+								<option value="finance">Finance</option>
+								<option value="psychology">Psychology</option>
+								<option value="other">Other</option>
+							</select>
+						</div>
+
+						{/* Submit button */}
 
 
-        </div>
-        {/* Submit button */}
-        <button type="submit" className="App-button" style={{ marginTop: '20px' }} disabled={!formValid}>
-          Next
-        </button>
-      </form>
-    </div>
-  );
+					</form>
+					</div>
+				</div>
+
+					<button type="submit" className="form-button" disabled={!formValid}>
+							Next
+					</button>
+			</div>
+		
+		</div>
+    );
 };
 
 export default DemographicPage;
